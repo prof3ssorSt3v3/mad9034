1a3904674a681ec027a1d3209567b1a75212ef04
bris0231 <54876624+bris0231@users.noreply.github.com>
2023-11-27 22:23:42 -0500

    course update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -59,6 +59,7 @@ module.exports = {
           "/modules/week10/",
           "/modules/week11/",
           "/modules/week12/",
+          "/modules/week13/",
           "/modules/week0/",
         ],
       },
